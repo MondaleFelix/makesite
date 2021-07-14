b8df7cf79bd0c917e9e1797787cc2be05ea126ab
Mondale Felix <mondalefelix@gmail.com>
2021-07-14 02:37:32 -0700

    rendered the contetns of first-post.text

--- makesite.go
+++ makesite.go
@@ -1,16 +1,36 @@
 package main
 
 import (
-
 	"fmt"
+	"html/template"
 	"io/ioutil"
-
+	"os"
 )
 
+
+type Text struct {
+	Paragraph string
+}
+
+
 func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+
+	// Reads content from text file
+	FileContents, err := ioutil.ReadFile("first-post.txt")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(string(fileContents))
+
+	// fmt.Print(string(FileContents))
+
+
+	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+
+	fmt.Print(t)
+	text := Text{Paragraph: string(FileContents)}
+	err = t.Execute(os.Stdout,text)
+
+	if err != nil {
+		panic(err)
+	}
 }
