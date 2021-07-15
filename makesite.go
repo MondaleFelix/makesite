113e9493cd36cfbbb34088eab84908c322c94f25
Mondale Felix <mondalefelix@gmail.com>
2021-07-14 19:16:20 -0700

    completed v1.1

--- makesite.go
+++ makesite.go
@@ -12,37 +12,51 @@ type Text struct {
 	Paragraph string
 }
 
-func main() {
-	var flagvar string
-
-	fmt.Print("Enter a file name: ")
-
-	flag.StringVar(&flagvar, "flagvar", "first-post.html", "Entered a file name ")
-	flag.Parse()
-	fmt.Print(flagvar)
-	// Reads content from text file
-	FileContents, err := ioutil.ReadFile(flagvar + ".txt")
-	if err != nil {
-		panic(err)
-	}
-
-	// fmt.Print(string(FileContents))
-
+func generateHTML(contents []byte, name string) {
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
 
 	fmt.Print(t)
-	text := Text{Paragraph: string(FileContents)}
-	err = t.Execute(os.Stdout, text)
+	text := Text{Paragraph: string(contents)}
+	err := t.Execute(os.Stdout, text)
 
 	if err != nil {
 		panic(err)
 	}
 
-	f, err := os.Create(flagvar + ".html")
+	f, err := os.Create(name + ".html")
 
 	err = t.Execute(f, text)
 
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	var flagvar string
+	var dir string
+
+	fmt.Print("Enter a file name: ")
+
+	flag.StringVar(&dir, "dir", ".", "Entered a directory ")
+
+	files, _ := ioutil.ReadDir(dir)
+
+	flag.StringVar(&flagvar, "flagvar", "first-post.html", "Entered a file name ")
+	flag.Parse()
+	fmt.Print(flagvar)
+	// Reads content from text file
+
+	for _, s := range files {
+		extention := s.Name()[len(s.Name())-4:]
+		name := s.Name()[:len(s.Name())-4]
+		if extention == ".txt" {
+			FileContents, err := ioutil.ReadFile(s.Name())
+			if err != nil {
+				panic(err)
+			}
+			generateHTML(FileContents, name)
+		}
+	}
+
 }
